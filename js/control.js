85730392a31c553905f81926cc2a8f0b2da00c74
Andrew Sykes <andrewsykes@btinternet.com>
2023-07-12 09:39:23 +0100

    advance

--- js/control.js
+++ js/control.js
@@ -1,32 +1,42 @@
-function controllerFactory(factor, extent, item, px = 0, state = false) {
-  let fraction = extent / factor,
-    Controller = function () {
-      this.inc = item * factor;
-      this.limit = this.inc * (extent / factor);
-    };
-}
-
 const control = document.querySelector(".control a"),
   slides = document.getElementById("slides"),
   extent = document.querySelectorAll(".slide").length,
   viewer = document.getElementById("viewer"),
   forward = document.getElementById("forward"),
   back = document.getElementById("back"),
+  item = document.querySelector(".slide").offsetWidth,
   always = (x) => () => x,
+  negate = (x) => () => !x,
+  doPartial = (flag) => {
+    return function p(f, ...args) {
+      if (f.length === args.length) {
+        return flag ? () => f(...args) : f(...args);
+      }
+      return (...rest) => p(f, ...args, ...rest);
+    };
+  },
+  doWhenPred = (pred, action) => {
+    return function (a, b) {
+      if (pred(a, b)) {
+        return action();
+      }
+    };
+  },
+  ptL = doPartial(),
+  defer = doPartial(true),
   curry3 = (fn) => (c) => (b) => (a) => fn(a, b, c),
-  best = (fun, coll, ...rest) => {
-    return coll.reduce((champ, contender) =>
-      fun(champ(...rest), contender(...rest)) ? champ : contender
-    );
-  };
+  invokeSub = (o, m, k, v) => o[m][k](v),
+  doKlas = ptL(invokeSub, viewer, "classList"),
+  doKlasDefer = defer(invokeSub, viewer, "classList"),
+  doShow = doKlas("add"),
+  doShowStart = doWhenPred((x) => !x, doKlasDefer("add")("start")),
+  doShowEnd = doWhenPred((a, b) => a !== b, doKlasDefer("add")("end")),
+  doHide = doKlas("remove");
 let px = 0,
   factor = 4,
   fraction = extent / factor,
-  item = document.querySelector(".slide").offsetWidth,
   inc = item * factor,
   limit = inc * fraction,
-  last,
-  state = false,
   apply = (pix) => {
     slides.classList.add("foo");
     slides.style.transform = `translate(${pix}px,0)`;
@@ -42,8 +52,10 @@ viewer.style.width = inc + "px";
 back.addEventListener("click", (e) => {
   e.preventDefault();
   if (validateButton(e.target)) {
+    doHide("end");
     px -= inc;
     px = Math.max(0, px);
+    doShowStart(px);
     apply(px * -1);
   }
 });
@@ -51,9 +63,12 @@ back.addEventListener("click", (e) => {
 forward.addEventListener("click", (e) => {
   e.preventDefault();
   if (validateButton(e.target)) {
-    if (doAdvance(px, limit, inc)) {
+    if (mayAdvance(px)) {
+      doHide("start");
       px += inc;
-      px = getNext(px, limit, inc);
+      let pix = doGetNext(px);
+      doShowEnd(px, pix);
+      px = pix;
       apply(px * -1);
     }
   }
