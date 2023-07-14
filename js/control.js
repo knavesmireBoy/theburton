2ab934fbcea4063ce11317bdc394b8862ec8b68f
Andrew Sykes <andrewsykes@btinternet.com>
2023-07-14 10:35:13 +0100

    fix inc limit1

--- js/control.js
+++ js/control.js
@@ -1,20 +1,18 @@
-
-const  tagTester = (name) => {
-  const tag = "[object " + name + "]";
-  return function (obj) {
-    return toString.call(obj) === tag;
-  };
-},
-isFunction = tagTester("Function"),
-    getRes = function (arg) {
-      if (isFunction(arg)) {
-        return arg();
-      }
-      return arg;
+const tagTester = (name) => {
+    const tag = "[object " + name + "]";
+    return function (obj) {
+      return toString.call(obj) === tag;
     };
+  },
+  isFunction = tagTester("Function"),
+  getRes = function (arg) {
+    if (isFunction(arg)) {
+      return arg();
+    }
+    return arg;
+  };
 
-
-function doGetComputedStyle (element, property) {
+function doGetComputedStyle(element, property) {
   const toCamelCase = function (variable) {
     return variable.replace(/-([a-z])/g, function (str, letter) {
       return letter.toUpperCase();
@@ -28,13 +26,8 @@ function doGetComputedStyle (element, property) {
     def = document.defaultView || window;
   if (typeof element.currentStyle !== "undefined") {
     computedStyle = element.currentStyle;
-  } else if (
-    def &&
-    def.getComputedStyle &&
-    isFunction(def.getComputedStyle)
-  ) {
-    computedStyle = def.getComputedStyle(element, property);
-    console.log(computedStyle);
+  } else if (def && def.getComputedStyle && isFunction(def.getComputedStyle)) {
+    computedStyle = def.getComputedStyle(element, null);
   }
   if (computedStyle) {
     try {
@@ -43,14 +36,10 @@ function doGetComputedStyle (element, property) {
         computedStyle.getPropertyValue(toCamelCase(property))
       );
     } catch (e) {
-      return (
-        computedStyle[property] ||
-        computedStyle[toCamelCase(property)]
-      );
+      return computedStyle[property] || computedStyle[toCamelCase(property)];
     }
   }
 }
-
 
 const always = (x) => () => x,
   negate = (x) => () => !x,
@@ -90,37 +79,47 @@ const always = (x) => () => x,
   doShowStart = doWhenPred((x) => !x, doKlasDefer("add")("start")),
   doShowEnd = doWhenPred((a, b) => a !== b, doKlasDefer("add")("end")),
   doHide = doKlas("remove"),
-  apply = (pix) => {
+  apply = (pix, flag = false) => {
+    //bit of a bodge
+    let i = 0,
+    inc = 1;
+    if(window.viewportSize.getWidth() < 1140){
+      i = (1280 / window.viewportSize.getWidth()) * 1.1;
+    }
+    let p = Math.ceil(pix) * (400 + i) / slides.clientWidth;
     all_slides.forEach((element) => {
-      element.style.transform = `translateX(${pix}px)`;
-      element.classList.add("foo");
+      element.style.transform = `translateX(${Math.ceil(p)}%)`;
+      if (!flag) element.classList.add("foo");
     });
   },
-  doAdvance = (px, limit, inc) => px !== limit - inc,
-  getNext = (px, limit, inc) => (px + inc > limit ? limit - inc : px),
+  doAdvance = (pix, lmt, incr) => pix !== lmt - incr,
+  getNext = (pix, lmt, incr) => (pix + incr > lmt ? lmt - incr : pix),
   mayAdvanceDefer = curry3(doAdvance),
   doGetNextDefer = curry3(getNext),
   validateButton = (tgt) => {
     return tgt.classList.contains("control");
   },
   getInc = () => {
-    let item = document.querySelector("#slides img").offsetWidth,
-    inc = item * factor,
-    limit = inc * fraction;
+    let item = document.querySelector("#slides img").clientWidth,
+      inc = item * factor,
+      limit = inc * fraction;
     return [inc, limit];
+  },
+  getOffset = () => {
+    let style = window.getComputedStyle(lead_slide, null),
+      values = style.transform.match(/-?\d+\.?\d*/g);
+    return values ? Math.abs(Math.ceil(values[4])) : 0;
   };
-let px = 0;
-//viewer.style.width = inc + "px";
+
+let px = getOffset();
 
 back.addEventListener("click", (e) => {
   e.preventDefault();
   if (validateButton(e.target)) {
-    let item = document.querySelector("#slides img").offsetWidth,
-    inc = item * factor,
-    limit = inc * fraction;
+    let [inc] = getInc();
     doHide("end");
     px -= inc;
-    px = Math.max(0, px);
+    px = Math.max(0, Math.ceil(px));
     doShowStart(px);
     apply(px * -1);
   }
@@ -130,13 +129,15 @@ forward.addEventListener("click", (e) => {
   e.preventDefault();
   if (validateButton(e.target)) {
     let [inc, limit] = getInc(),
-    mayAdvance = mayAdvanceDefer(inc)(limit),
-    doGetNext = doGetNextDefer(inc)(limit);
-    console.log(inc, limit);
+      mayAdvance = mayAdvanceDefer(inc)(limit),
+      doGetNext = doGetNextDefer(inc)(limit);
+    //https://stackoverflow.com/questions/11160227/translate-x-and-y-percentage-values-based-on-elements-height-and-width
+    //https://stackoverflow.com/questions/43065579/how-to-convert-css-transform-matrix-back-to-its-component-properties
+    px = getOffset();
     if (mayAdvance(px)) {
       doHide("start");
       px += inc;
-      let pix = doGetNext(px);
+      let pix = doGetNext(Math.ceil(px));
       doShowEnd(px, pix);
       px = pix;
       apply(px * -1);
@@ -149,5 +150,29 @@ slides.addEventListener("click", (e) => {
   if (e.target.nodeName === "IMG") {
     let src = e.target.getAttribute("src");
     $("preview").setAttribute("src", src);
+  }
+});
+
+let rtime,
+  timeout = false,
+  delta = 200,
+  win = 0;
+
+function resizeend() {
+  if (new Date() - rtime < delta) {
+    setTimeout(resizeend, delta);
+  } else {
+    timeout = false;
+    let w = window.viewportSize.getWidth() / win;
+    //  px *= w;
+  }
+}
+
+window.addEventListener("resize", () => {
+  rtime = new Date();
+  if (timeout === false) {
+    win = window.viewportSize.getWidth();
+    timeout = true;
+    setTimeout(resizeend, delta);
   }
 });
